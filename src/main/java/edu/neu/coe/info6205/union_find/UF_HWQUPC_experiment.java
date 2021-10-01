ebf4363adb5f93c818ecb33277f136cdbe6195fa
Kai Yang Hsieh <hsieh.k@northeastern.edu>
2021-10-01 10:35:27 -0400

    feature: add experiment code for step 2 and step 3

---
+++ src/main/java/edu/neu/coe/info6205/union_find/UF_HWQUPC_experiment.java
@@ -0,0 +1,41 @@
+package edu.neu.coe.info6205.union_find;
+
+import java.util.Random;
+
+public class UF_HWQUPC_experiment {
+
+    public static void main(String[] args) {
+        boolean pathCompression = true;
+        int[] numberOfSites = {100, 200, 400, 800, 1600, 3200, 6400, 12800, 25600};
+        int times = 10;
+
+        for (int i = 0; i < numberOfSites.length; i++) {
+            int connections = 0;
+            for (int j = 0; j < times; j++) {
+                UF_HWQUPC union = new UF_HWQUPC(numberOfSites[i], pathCompression);
+                connections += count(union);
+            }
+            System.out.println("Average " + connections / times + " connections of " + numberOfSites[i] + " sites");
+        }
+    }
+
+    private static int count(UF_HWQUPC union) {
+        int connections = 0;
+        while (union.components() != 1) {
+            int[] randomPair = getRandomPair(union.size());
+            if (!union.connected(randomPair[0], randomPair[1])) {
+                union.connect(randomPair[0], randomPair[1]);
+                connections++;
+            }
+
+        }
+
+        return connections;
+    }
+
+    private static int[] getRandomPair(int max) {
+        Random random = new Random();
+        int[] randomPair = {random.nextInt(max), random.nextInt(max)};
+        return randomPair;
+    }
+}
